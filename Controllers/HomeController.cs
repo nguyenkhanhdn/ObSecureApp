e39c755b56287d78e02461b33d9b8a8171dda4d0
khanhn2 <khanhn@fpt.edu.vn>
2024-11-14 21:15:09 +0700

    add-new

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ObSecureApp.Models;
 using System.Diagnostics;
+using System.IO;
 
 namespace ObSecureApp.Controllers
 {
@@ -18,16 +19,108 @@ namespace ObSecureApp.Controllers
             return View();
         }
 
-        public IActionResult About()
+        [HttpGet]
+        public IActionResult Predict()
         {
+
+            ////Load sample data
+            //var sampleData = new ObSecureMLModel.ModelInput()
+            //{
+            //    Comment = @"The other two films Hitch and Magnolia are also directly related to the community in question, and may be of interest to those who see those films.  So why not link to them?",
+            //};
+
+            ////Load model and predict output
+            //var result = ObSecureMLModel.Predict(sampleData);
+            //ViewBag.Result = result.PredictedLabel.ToString() + result.Label + result.Score.ToString();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Predict(string id)
+        {
+
+            //Load sample data
+            var sampleData = new ObSecureMLModel.ModelInput()
+            {
+                Comment = @"The other two films Hitch and Magnolia are also directly related to the community in question, and may be of interest to those who see those films.  So why not link to them?",
+            };
+
+            //Load model and predict output
+            var result = ObSecureMLModel.Predict(sampleData);
+            ViewBag.Result = result.PredictedLabel.ToString() + result.Label + result.Score.ToString();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Predict2()
+        {
+
+            ////Load sample data
+            //var sampleData = new ObSecureMLModel.ModelInput()
+            //{
+            //    Comment = @"The other two films Hitch and Magnolia are also directly related to the community in question, and may be of interest to those who see those films.  So why not link to them?",
+            //};
+
+            ////Load model and predict output
+            //var result = ObSecureMLModel.Predict(sampleData);
+            //ViewBag.Result = result.PredictedLabel.ToString() + result.Label + result.Score.ToString();
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Predict2(IFormFile fileUpload)
+        {
+            if (ModelState.IsValid)
+            {
+                if (fileUpload != null && fileUpload.Length > 0)
+                {
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", fileUpload.FileName);
+                    //Using Buffering
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        // The file is saved in a buffer before being processed
+                        await fileUpload.CopyToAsync(stream);
+                    }
+
+                    //Using Streaming
+                    //using (var stream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                    //{
+                    //    await SingleFile.CopyToAsync(stream);
+                    //}
+                    // Process the file here (e.g., save to the database, storage, etc.)
+
+                    
+
+                    // Create single instance of sample data from first line of dataset for model input
+                    var imageBytes = System.IO.File.ReadAllBytes(filePath);
+                    ObSecureMLModel2.ModelInput sampleData = new ObSecureMLModel2.ModelInput()
+                    {
+                        ImageSource = imageBytes,
+                    };
+
+                    // Make a single prediction on the sample data and print results.
+                    var sortedScoresWithLabel = ObSecureMLModel2.Predict(sampleData);
+                    ViewBag.Result = sortedScoresWithLabel.PredictedLabel.ToString() + sortedScoresWithLabel.Label + sortedScoresWithLabel.Score.ToString();
+                    return View();
+                }
+            }
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+
+        }
         public IActionResult Contact()
         {
             return View();
         }
-
+        public IActionResult Features()
+        {
+            return View();
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
